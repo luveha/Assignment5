d8925fb36b8f1e0e43583696f3c8735573d6da58
Lucas <lucasveha@gmail.com>
2025-03-06 16:11:51 +0100

    some of the first excer

--- Memory.fs
+++ Memory.fs
@@ -1,10 +1,14 @@
 module Interpreter.Memory
     
-    type memory = unit // put your own type here
+    type memory = {m: Map<int,int>; next: int}
 
-    let empty _ = failwith "not implemented"
+    let empty memsize = {m = Map.empty<int,int>; next=0}
 
-    let alloc _ = failwith "not implemented"
+    let rec alloc size mem =
+        match size with
+        | 1 -> Some({m= mem.m.Add(size,0);next=mem.next+1},mem.next+1)
+        | s when s > 0 -> alloc (s-1) {m = mem.m.Add(size,0); next=mem.next+1}
+        | _ -> None
     
     let free _ = failwith "not implemented"
         
