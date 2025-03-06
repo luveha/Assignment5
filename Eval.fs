7c12b61157e9a480b22c69630d04e5fe7cf7b995
Lucas <lucasveha@gmail.com>
2025-03-06 15:25:20 +0100

    fixes

--- Eval.fs
+++ Eval.fs
@@ -4,6 +4,86 @@ module Interpreter.Eval
     open Language
     open State
     
-    let arithEval _ = failwith "not implemented"    
-    let boolEval _ = failwith "not implemented"
-    let stmntEval _ = failwith "not implemented"
+    
+    let rec arithEval a st = 
+        match a with
+        | Num n -> Ok n
+        | Var v when v = "random" -> Ok 7 //7 is always random
+        | Var v ->
+                getVar v st
+        | Add (x,y) ->
+                match arithEval x st, arithEval y st with
+                | Ok x, Ok y -> Ok (x + y)
+                | Error e, _ | _ , Error e-> Error(e)
+        | Mul (x,y) ->
+                match arithEval x st, arithEval y st with
+                | Ok x, Ok y -> Ok (x * y)
+                | Error e, _ | _ , Error e-> Error(e)
+        | Div (x,y) ->
+                match arithEval x st, arithEval y st with
+                | Error e, _ | _ , Error e-> Error(e)
+                | Ok x, Ok 0 -> Error (DivisionByZero) 
+                | Ok x, Ok y -> Ok (x / y)
+        | Mod (x,y)-> 
+                match arithEval x st, arithEval y st with
+                | Error e, _ | _ , Error e-> Error(e)
+                | Ok x, Ok 0 -> Error (DivisionByZero) 
+                | Ok x, Ok y -> Ok (x % y)     
+
+    let rec boolEval b st = 
+        match b with
+        | TT -> Ok true
+        | Eq (a,c)-> 
+                match arithEval a st, arithEval c st with
+                | Error e, _ | _, Error e -> Error(e)
+                | Ok x,Ok y -> Ok (x = y)  
+        | Lt (a,c) -> 
+                match arithEval a st, arithEval c st with
+                | Error e, _ | _, Error e -> Error(e)
+                | Ok x,Ok y -> Ok (x < y) 
+        | Conj (a,c) -> 
+                match boolEval a st, boolEval c st with
+                | Error e, _ | _, Error e -> Error(e)
+                | Ok x,Ok y -> Ok (x && y) 
+        | Not a -> 
+                match boolEval a st with
+                | Error e -> Error(e)
+                | Ok x -> Ok (not x)
+
+    let rec stmntEval s st = 
+        match s with 
+        | Skip -> Ok st
+        | Declare s -> 
+                match declare s st with
+                | Error e -> Error e
+                | Ok st -> Ok st
+        | Assign(v,a) -> 
+                match arithEval a st with
+                | Error e-> Error e
+                | Ok x -> 
+                        match setVar v x st with
+                        | Ok x -> Ok x
+                        | Error(e) -> Error(e)
+        | Seq (s1,s2) -> 
+                match stmntEval s1 st with
+                | Error e -> Error e
+                | Ok st' -> 
+                        match stmntEval s2 st' with
+                        | Error e -> Error e
+                        | Ok st'' -> Ok st'' 
+        | If (gaurd, s1, s2) ->
+                match boolEval gaurd st with
+                | Error e -> Error e
+                | Ok true -> stmntEval s1 st
+                | Ok false -> stmntEval s2 st
+        | While (gaurd, s') -> 
+                match boolEval gaurd st with
+                | Error e -> Error e
+                | Ok true -> 
+                        match stmntEval s' st with
+                        | Error e -> Error e
+                        | Ok st'' -> 
+                                match stmntEval (While (gaurd, s')) st'' with
+                                | Error e -> Error e
+                                | Ok st''' -> Ok st'''
+                | Ok false -> Ok st
--- State.fs
+++ State.fs
@@ -3,23 +3,52 @@ module Interpreter.State
     open Result
     open Language
     
-    let validVariableName _ = failwith "not implemented"
-    let reservedVariableName _ = failwith "not implemented"
+    let reservedVariableNameList = ["if"; "then"; "else"; "while"; "declare"; "print"; "random"; "fork"; "__result__"]
+    let reservedVariableName s= List.exists(fun x -> x = s) reservedVariableNameList
+
+    let validVariableName (s: string) = 
+        match s.[0] with
+        | c when System.Char.IsAsciiLetter(c) || c = '_' -> String.forall(fun c-> System.Char.IsAsciiLetterOrDigit(c) || c = '_') s.[1..]
+        | _ -> false
     
-    type state = unit // your type goes here
     
-    let mkState _ = failwith "not implemented"
+    type state = {m: List<Map<string,int>>}
+    let mkState () = {m = [Map.empty<string,int>]}
+
+    let push st = {m = Map.empty<string,int> :: st.m}
+
+    let pop st = 
+        match st.m with
+        | t :: h -> {m = h}
+        | _ -> failwith "List is empty"  
+    
+
+    let declare x st =
+        match st.m.Head with
+        | m when m.ContainsKey x -> 
+            Error (VarNotDeclared(x)) //HERE
+        | _ when reservedVariableName x -> 
+            Error (ReservedName(x))
+        | _ when not (validVariableName x) -> 
+            Error (InvalidVarName(x))
+        | m ->
+            Ok ({m = m.Add(x,0) :: (pop st).m})
+
+    let rec getVar x st =
+        match st.m with
+        | [] -> Error (VarNotDeclared(x))
+        | h :: t ->
+            match h.ContainsKey x with
+            | true -> Ok h.[x]
+            | _ -> getVar x {m = t}
+
+    let rec setVar x v st = 
+        match st.m with
+        | [] -> Error (VarNotDeclared(x))
+        | h :: t ->
+            match h.ContainsKey x with
+            | true -> Ok {m = h.Add(x,v) :: t}
+            | _ -> setVar x v {m = t}
+
+
     let random _ = failwith "not implemented"
-    
-    let declare _ = failwith "not implemented"
-    
-    let getVar _ = failwith "not implemented"
-    let setVar _ = failwith "not implemented"
-    
-    let alloc _ = failwith "not implemented"
-    let free _ = failwith "not implemented"
-    let getMem _ = failwith "not implemented"
-    let setMem _ = failwith "not implemented"
-    
-    let push _ = failwith "not implemented"
-    let pop _ = failwith "not implemented"     
